434b0eae801e790337e2ddccbc0a593ce60e1253
Yunus emre Aktas <yaktas@k2m05s03.42kocaeli.com.tr>
2022-05-14 16:03:34 +0300

    taqs start

---
+++ taqs/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" type="text/css" href="triangle.css"/>
+	<title>Taqs</title>
+</head>
+<body>
+
+<div id="container" class="container" style="overflow-y: hidden;">
+
+	<div id="output" class="container" style="position:absolute; width:100%; z-index: -1;" ></div>
+
+		<div class="header">
+			<div class="baslik">
+				Taqs
+			</div>
+			<div class="altbaslik">
+				Truth and Quality Standarts
+			</div>
+		</div>
+
+
+		<div class="navbar">
+			
+			<a href="#" class="navbar_href">
+				<div class="navbar_item">
+					Home
+				</div>
+			</a>
+
+			<a href="#" class="navbar_href">
+				<div class="navbar_item">
+					FAQ
+				</div>
+			</a>
+
+			<a href="#" class="navbar_href">
+				<div class="navbar_item">
+					API
+				</div>
+			</a>
+		</div>
+
+	
+		<div class="body">
+
+			<div class="text_panel">
+				<div class="text_header">
+					Visyonumuz
+				</div>
+				<div class="text_content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas odit consequatur quidem quod quae deserunt harum voluptas odio animi dolores illum, inventore nobis eaque magni corporis. Dolores consectetur laborum iure.
+				</div>
+			</div>
+
+			<div class="text_panel">
+				<div class="text_header">
+					Visyonumuz
+				</div>
+				<div class="text_content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas odit consequatur quidem quod quae deserunt harum voluptas odio animi dolores illum, <br><br> inventore nobis eaque magni corporis. Dolores consectetur laborum iure. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, quae doloremque voluptatum deleniti soluta magnam quibusdam deserunt nisi natus. Pariatur architecto saepe numquam omnis officia eveniet soluta rerum maiores iusto?
+				</div>
+			</div>
+				
+			
+		</div>
+</div>
+
+<div id="controls" class="controls" style="display: none;"></div>
+	
+	<script src="triangle.js"></script>
+</body>
+</html>
